1661ce8bda26e044b6e81337eec086e66b1ce997
soleddarity <119897250+soleddarity@users.noreply.github.com>
2022-12-14 21:12:58 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
-
-module.exports = {
-  target: "serverless",
-};

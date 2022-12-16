977db932c0ec2a1315922bb33ff256740d863074
soleddarity <119897250+soleddarity@users.noreply.github.com>
2022-12-16 22:13:24 +0100

    Update staking.tsx

--- src/pages/staking.tsx
+++ src/pages/staking.tsx
@@ -14,31 +14,31 @@ import { useWallet } from "@solana/wallet-adapter-react";
 import Login from "@/components/Login";
 import { LibraryIcon } from "@heroicons/react/outline";
 
-//const Staking = () => {
-//  const { publicKey } = useWallet();
-//  return (
-//    <Main meta={<Meta title="Toolidarity" description="" />}>
-//      {publicKey ? (
-//        <>
-//          <Layout>
-//            <div className="flex flex-col gap-5">
-//              <h1 className="text-4xl flex items-center gap-2 font-semibold">
-//                <LibraryIcon className="h-10 w-10"></LibraryIcon> Staking
-//              </h1>
-//              {
-//                //@ts-ignore
-//                <div id="diamond-vaults" vaultPath="dudes"></div>
-//              }
-//            </div>
-//          </Layout>
-//        </>
-//      ) : (
-//        <>
-//          <Login></Login>
-//        </>
-//      )}
-//    </Main>
-//  );
-//};
+const Staking = () => {
+  const { publicKey } = useWallet();
+  return (
+    <Main meta={<Meta title="Toolidarity" description="" />}>
+      {publicKey ? (
+        <>
+          <Layout>
+            <div className="flex flex-col gap-5">
+              <h1 className="text-4xl flex items-center gap-2 font-semibold">
+                <LibraryIcon className="h-10 w-10"></LibraryIcon> Staking
+              </h1>
+              {
+                //@ts-ignore
+                <div id="diamond-vaults" vaultPath="dudes"></div>
+              }
+            </div>
+          </Layout>
+        </>
+      ) : (
+        <>
+          <Login></Login>
+        </>
+      )}
+    </Main>
+  );
+};
 
 export default Staking;

300b3deddc5f680d9ea0241c241ded08b688f512
soleddarity <119897250+soleddarity@users.noreply.github.com>
2022-12-22 18:19:07 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -646,7 +646,7 @@ const Index = () => {
               <Layout>
                 <div className="flex flex-col gap-2 lg:p-5">
                   <div className="flex items-center flex-col xl:flex-row justify-between">
-                    <div className="overflow-scroll flex flex-col gap-2 lg:p-5">
+                    <div className="flex flex-col gap-2 lg:p-5">
                       <div className="bg-[#171E27] font-semibold flex items-center border border-[#2f3a47] rounded-xl">
                         <div
                           onClick={() => settypeTools("Free tools")}

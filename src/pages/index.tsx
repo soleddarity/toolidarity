c92e804c82b3874f499d83283dafc4bda2506801
soleddarity <119897250+soleddarity@users.noreply.github.com>
2023-02-14 16:42:29 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -639,6 +639,18 @@ const Index = () => {
   ];
 
   const discounts = [
+ {
+      name: "Pixel Art Service",
+      description:
+        "Get the best generative pixel art for your NFT collection in GIF or PNG designed by Sans-Qui",
+      icon: "https://pbs.twimg.com/profile_images/1613851543769423874/yO1ObGXb_400x400.jpg",
+      twitter: "https://twitter.com/dudes_sol",
+      discord: "https://discord.com/invite/dudessol",
+      website: "",
+      category: "Art Creation",
+      from: "#ffd623",
+      to: "#ffffff",
+    },
     {
       name: "Oak Paradise",
       description:

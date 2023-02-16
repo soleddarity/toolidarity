57ccb79c0fcbaa9b71be21f0537642091d493b8c
soleddarity <119897250+soleddarity@users.noreply.github.com>
2023-02-16 01:36:44 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,8 @@ import "aos/dist/aos.css";
 import { ContextProvider } from "@/contexts/ContextProvider";
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider } from "next-themes";
+import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 require("@solana/wallet-adapter-react-ui/styles.css");
 
@@ -28,6 +30,7 @@ function MyApp({ Component, pageProps }) {
         <NextUIProvider>
           <ThemeProvider enableSystem={true} attribute="class">
             <Component {...pageProps} />
+           <Analytics />
           </ThemeProvider>
         </NextUIProvider>
       </ContextProvider>

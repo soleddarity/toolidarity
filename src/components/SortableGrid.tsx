c3e7c7f4c72abc94d9c818247b019ccdf0f6b8ed
voidmanevoid <voidarelli@protonmail.com>
2023-02-19 09:39:50 -0700

    disable sorting if touch device is detected

--- src/components/SortableGrid.tsx
+++ src/components/SortableGrid.tsx
@@ -31,6 +31,12 @@ const SortableGrid = (props: any) => {
     };
 
     useEffect(() => {
+        if (( 'ontouchstart' in window ) ||
+           ( navigator.maxTouchPoints > 0 ) ||
+           ( navigator.msMaxTouchPoints > 0 )) {
+            return;
+        }
+        
         sortableJsRef.current = new Sortable(gridRef.current, {
         animation: 150,
         filter: ".static",

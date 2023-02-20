290a4d412c8b76f949f103f99984e72490dd2beb
soleddarity <119897250+soleddarity@users.noreply.github.com>
2023-02-20 10:27:59 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -910,6 +910,18 @@ const Index = () => {
       from: "#4D4D4D",
       to: "#AEAEAE",
 },
+{
+      name: "The Dudes Cards",
+      description:
+        "667 Dudes Cards gives you access to our exclusive AMM rev-share scheme!",
+      icon: "https://eemrlvzcxfcudiminbgx.supabase.co/storage/v1/object/public/assets/raffles/8f367b023357.webp",
+      twitter: "https://twitter.com/dudes_sol",
+      discord: "https://discord.com/dudessol",
+      website: "https://toolidarity.app/",
+      category: "Incoming",
+      from: "#da5b34",
+      to: "#050505",
+    },
   ];
 
   const navigation = [

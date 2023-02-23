cffb30b153ddbbf9c87a828c618cf9cb021a6d24
soleddarity <119897250+soleddarity@users.noreply.github.com>
2023-02-23 09:50:22 +0100

    Update tool.ts

--- src/tools/ethereum/tool.ts
+++ src/tools/ethereum/tool.ts
@@ -113,7 +113,7 @@ export const tools = [
 {
     name: "Pixel Art Service",
     description:
-      "Get the best generative pixel art for your NFT collection in GIF or PNG designed by Sans-Qui",
+      "Get the best generative pixel art for your NFT collection in GIF or PNG designed by Sans-Qui. -25% for our holders.",
     icon: "https://pbs.twimg.com/profile_images/1613851543769423874/yO1ObGXb_400x400.jpg",
     twitter: "https://twitter.com/dudes_sol",
     discord: "https://discord.com/invite/dudessol",

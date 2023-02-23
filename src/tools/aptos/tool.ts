4a75d25fa290fae04b0ceb5fe5716e66d91322b5
soleddarity <119897250+soleddarity@users.noreply.github.com>
2023-02-23 09:51:32 +0100

    Update tool.ts

--- src/tools/aptos/tool.ts
+++ src/tools/aptos/tool.ts
@@ -7,7 +7,18 @@ export const tools = [
   ];
 
   export const discounts = [
-
+{
+    name: "Pixel Art Service",
+    description:
+      "Get the best generative pixel art for your NFT collection in GIF or PNG designed by Sans-Qui. -25% for our holders.",
+    icon: "https://pbs.twimg.com/profile_images/1613851543769423874/yO1ObGXb_400x400.jpg",
+    twitter: "https://twitter.com/dudes_sol",
+    discord: "https://discord.com/invite/dudessol",
+    website: "",
+    category: "Art Creation",
+    from: "#070100",
+    to: "#fefcfc",
+  },
   ];
 
   export const games = [

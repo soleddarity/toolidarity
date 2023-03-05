f830d3f4ee9c697519a52c449c2e4c61ef82db23
soleddarity <119897250+soleddarity@users.noreply.github.com>
2023-03-05 16:36:05 +0100

    Update tool.ts

--- src/tools/polygon/tool.ts
+++ src/tools/polygon/tool.ts
@@ -144,18 +144,6 @@ export const tools = [
 
   export const Holderstools = [
 {
-    name: "Pixel Art Service",
-    description:
-      "Get the best generative pixel art for your NFT collection in GIF or PNG designed by Sans-Qui. -25% for our holders.",
-    icon: "https://pbs.twimg.com/profile_images/1613851543769423874/yO1ObGXb_400x400.jpg",
-    twitter: "https://twitter.com/dudes_sol",
-    discord: "https://discord.com/invite/dudessol",
-    website: "",
-    category: "Art Creation",
-    from: "#070100",
-    to: "#fefcfc",
-  },
-{
     name: "PolygonBoyz",
     description:
       "The PolygonBoyz collection is built on the foundation of community and trust a bond stronger than any virtual world's pixels and code. It's a bond that has propelled us to new heights and allowed us to embark on epic adventures!",
